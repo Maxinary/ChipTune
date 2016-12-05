aa77f45f547b02ea50396df31cc1b4dea33e4fdf
Maxinary <max.scribs@gmail.com>
2016-12-04 20:26:07 -0500

    Revert "Fixed audio output to wav"
    This reverts commit 757a7dbcca7a10483177f792570aa9628198ed0c.

--- ChipTune.py
+++ ChipTune.py
@@ -3,21 +3,15 @@ import numpy
 import pyaudio
 import copy
 import re
-import struct
 import wave
 
 class ChipTune:
-    def square(frequency, length, volume, rate, offset):
+    def square(frequency, length, volume, rate):
         length = int(length * rate)
         factor = float(frequency) * (math.pi * 2) / rate
-        k = [volume if x%(2*math.pi)>math.pi else -volume for x in (numpy.arange(length)+offset) * factor]
+        k = [volume if x%(2*math.pi)>math.pi else -volume for x in numpy.arange(length) * factor]
         return k
 
-    def triangle(frequency, length, volume, rate, offset):
-        length = int(length * rate)
-        factor = float(frequency) * (math.pi * 2) / rate
-        k = [volume-2*volume*(x%(2*math.pi)-math.pi)/math.pi if x%(2*math.pi)>math.pi else -volume+2*volume*(x%(2*math.pi))/math.pi for x in (numpy.arange(length)+offset) * factor]
-        return k
 
     def play_tone(stream, frequency=440, length=1, volume=.5, rate=44100):
         chunks = []
@@ -49,40 +43,39 @@ class ChipTune:
         "B" :246.94
     }
 
-    def playNotes(nin,r=0.2):
+    def playNotes(n1=[],n2=[],n3=[],r=0.2):
         p = pyaudio.PyAudio()
         chunks = []
         intermediate = []
-        for i in range(len(nin)):#iterate through the different "instrument melodies" in the input
-            h = nin[i]#the current Melody
-            intermediate.append([])
-            for j in range(len(h)):#for every note/rest in that melody
-                #Turn it into a tick array of volume pos at that point
-                if h[j].hertz < ChipTune.Notes["C"]:#play the low notes as triangle waves
-                    intermediate[i] += ChipTune.triangle(h[j].hertz, r*h[j].length, h[j].volume, 44100, 0)
-                else:#high notes are square
-                    intermediate[i] += ChipTune.square(h[j].hertz, r*h[j].length, h[j].volume, 44100, 0)
-        for i in range(max([len(k) for k in intermediate])):#go through all the notes and combine the melody
+        for i in range(3):
+            h = [n1,n2,n3][i]
+            if h!=[]:
+                intermediate.append([])
+                for j in range(len(h)):
+                    intermediate[i] += ChipTune.square(h[j].hertz, r*h[j].length, h[j].volume, 44100)
+        for i in range(max([len(k) for k in intermediate])):
             sume = 0
-            for j in intermediate:#iterate through all of the notes for that tick
-                if i < len(j):#make sure that melody hasnt ended yet
-                    sume += j[i]#add it to the thing
-            chunks.append([sume/len(nin)*0.2])#scale the volume
-        chunk = numpy.concatenate(chunks)
+            count = 0
+            for j in intermediate:
+                if i < len(j):
+                    sume += j[i]
+                    count += 1
+            chunks.append([sume/count])
+        chunk = numpy.concatenate(chunks) * 0.25
         
         stream = p.open(format=pyaudio.paFloat32,
-                    channels=1, rate=44100, output=1)
+                    channels=1, rate=44100, output=1
+                        ,frames_per_buffer=1024)
         
-        with wave.open("out.wav", "wb") as w:#write to file
+        with wave.open("out.wav", "wb") as w:
             w.setnchannels(1)
-            w.setsampwidth(p.get_sample_size(pyaudio.paInt16))
-            w.setframerate(44100.0)
+            w.setsampwidth(p.get_sample_size(pyaudio.paFloat32))
+            w.setframerate(44100)
+            w.writeframes(chunk.astype(numpy.float32).tostring())
 
-            intVec = numpy.vectorize(lambda x: int(x))
-            w.writeframes(intVec(chunk * 0.1 * 2**31).astype(numpy.int16).tostring())
-            
         stream.write(chunk.astype(numpy.float32).tostring())
 
+        #write to file
         stream.close()
         p.terminate()
 
@@ -100,6 +93,7 @@ class ChipTune:
         k = []
         with open(filename,"r") as f:
             k = "".join(f.readlines()).replace("\n"," ").split("+")
+            print(k)
         ar = []
         num = re.compile("\.\-?\d*(\.\d*)?")
         reg = re.compile("(\.\-?\d*(\.\d*)?)?(O|([ABCDEFG]\#?\d(\,\d\d)?))")
@@ -107,7 +101,6 @@ class ChipTune:
             ar.append([])
             for j in i.split(" "):
                 if reg.match(j):
-                    print(j)
                     if j[len(j)-1] == "\n":
                         j = j[:-1]
                     l=1
@@ -141,8 +134,8 @@ class ChipTune:
                     
 
 if __name__ == "__main__":
-    r = 1/5
-    a = ChipTune.fileToNotes("in.txt")
+    r = 1/9
+    a = ChipTune.fileToNotes("USA.txt")
     while len(a)<3:
         a.append([])
-    ChipTune.playNotes(a,r)
+    ChipTune.playNotes(a[0],a[1],a[2],r)
